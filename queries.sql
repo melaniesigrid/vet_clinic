f094fda2520d23604e6c402769adc7288cbcdf85
Melanie Arellano <melaniesigrid@protonmail.com>
2022-03-30 19:45:43 -0600

    Complete multiple queries exercise

--- queries.sql
+++ queries.sql
@@ -47,3 +47,47 @@ SELECT AVG(weight_kg) FROM animals;
 SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' GROUP BY species;
+
+/* Write queries (using JOIN) to answer the following questions:
+- What animals belong to Melody Pond?
+- List of all animals that are pokemon (their type is Pokemon).
+- List all owners and their animals, remember to include those that don't own any animal.
+- How many animals are there per species?
+- List all Digimon owned by Jennifer Orwell.
+- List all animals owned by Dean Winchester that haven't tried to escape.
+- Who owns the most animals? */
+
+SELECT name FROM animals A
+JOIN owners O ON A.owner_id = O.id
+WHERE O.full_name = 'Melody Pond';
+
+SELECT A.name FROM animals A
+JOIN species S ON A.species_id = S.id
+WHERE S.name = 'Pokemon';
+
+SELECT full_name, name
+FROM animals A
+FULL OUTER JOIN owners O on O.id = A.owner_id;
+
+SELECT S.name, COUNT(*) FROM animals A
+JOIN species S ON A.species_id = S.id
+GROUP BY S.name;
+
+SELECT A.name
+FROM animals A
+JOIN owners O ON A.owner_id = O.id
+JOIN species S ON A.species_id = S.id
+WHERE O.full_name = 'Jennifer Orwell'
+AND S.name = 'Digimon';
+
+SELECT A.name
+FROM animals A
+JOIN owners O ON A.owner_id = O.id
+WHERE A.escape_attempts = 0
+AND O.full_name = 'Dean Winchester';
+
+SELECT COUNT(*) as count, full_name
+FROM animals as A
+JOIN owners O ON A.owner_id = O.id
+GROUP BY full_name
+ORDER BY count desc;
--- schema.sql
+++ schema.sql
@@ -16,3 +16,42 @@ CREATE TABLE animals (
 /* Add column species of type string */
 ALTER TABLE animals
 ADD COLUMN species TEXT;
+
+/* Create a table named owners with the following columns:
+-id: integer (set it as autoincremented PRIMARY KEY)
+- full_name: string
+- age: integer */
+
+CREATE TABLE owners (
+  id         INT GENERATED ALWAYS AS IDENTITY,
+  full_name   TEXT,
+  age        INT,
+  PRIMARY KEY(id)
+);
+
+/* Create a table named species with the following columns:
+- id: integer (set it as autoincremented PRIMARY KEY)
+- name: string */
+
+CREATE TABLE species (
+  id    INT GENERATED ALWAYS AS IDENTITY,
+  name  TEXT,
+  PRIMARY KEY(id)
+);
+
+/* Modify animals table:
+- Make sure that id is set as autoincremented PRIMARY KEY
+- Remove column species
+- Add column species_id which is a foreign key referencing species table
+- Add column owner_id which is a foreign key referencing the owners table */
+
+ALTER TABLE animals
+DROP COLUMN species,
+ADD COLUMN species_id INT,
+ADD CONSTRAINT species_key
+  FOREIGN KEY (species_id)
+    REFERENCES species(id),
+ADD COLUMN owner_id INT,
+ADD CONSTRAINT owner_key
+  FOREIGN KEY (owner_id)
+    REFERENCES owners(id);
